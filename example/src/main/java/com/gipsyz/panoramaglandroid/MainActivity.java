d3cc9c32d1eacd65f490a01d50172b524089bb66
Marian C Moldovan <moldovan0991@gmail.com>
2015-09-20 01:26:02 +0200

    Increased ver number

--- example/src/main/java/com/gipsyz/panoramaglandroid/MainActivity.java
+++ example/src/main/java/com/gipsyz/panoramaglandroid/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     PLManager plManager;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
