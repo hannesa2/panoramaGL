7c6ad6af8ab0e4784c640faaaaa0cb1e6f65b6da
walpokes <lespinsideg@gmail.com>
2016-11-11 00:52:03 +0900

    update example : add buttons that change panorama images.

--- example/src/main/java/com/gipsyz/panoramaglandroid/MainActivity.java
+++ example/src/main/java/com/gipsyz/panoramaglandroid/MainActivity.java
@@ -1,9 +1,13 @@
 package com.gipsyz.panoramaglandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.panoramagl.PLICamera;
 import com.panoramagl.PLImage;
 import com.panoramagl.PLManager;
 import com.panoramagl.PLSphericalPanorama;
@@ -11,24 +15,44 @@ import com.panoramagl.utils.PLUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    PLManager plManager;
+    private PLManager plManager;
+    private int currentIndex = -1;
+    private int[] resourceIds = new int[]{ R.raw.sighisoara_sphere, R.raw.sighisoara_sphere_2};
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.button_1:
+                    changePanorama(0);
+                    break;
+                case R.id.button_2:
+                    changePanorama(1);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         plManager = new PLManager(this);
-        plManager.setContentView(R.layout.activity_main);
+        plManager.setContentView((ViewGroup)findViewById(R.id.content_view));
         plManager.onCreate();
 
         plManager.setAccelerometerEnabled(false);
         plManager.setInertiaEnabled(false);
         plManager.setZoomEnabled(false);
-        
-        PLSphericalPanorama panorama = new PLSphericalPanorama();
-        panorama.getCamera().lookAtAndZoomFactor(5.f, 0f, 0.8f, false);
 
-        panorama.setImage(new PLImage(PLUtils.getBitmap(this, R.raw.sighisoara_sphere), false));
-        plManager.setPanorama(panorama);
+        changePanorama(0);
+
+        Button button1 = ((Button) findViewById(R.id.button_1));
+        Button button2 = ((Button) findViewById(R.id.button_2));
+        button1.setOnClickListener(buttonClickListener);
+        button2.setOnClickListener(buttonClickListener);
     }
 
     @Override
@@ -51,5 +75,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         return plManager.onTouchEvent(event);
+    }
+
+    private void changePanorama(int index) {
+        if(currentIndex == index) return;
+
+        PLSphericalPanorama panorama = new PLSphericalPanorama();
+        panorama.setImage(new PLImage(PLUtils.getBitmap(this, resourceIds[index]), false));
+        float pitch = 5f;
+        float yaw = 0f;
+        float zoomFactor = 0.8f;
+
+        if(currentIndex != -1) {
+            PLICamera camera = plManager.getPanorama().getCamera();
+            pitch = camera.getPitch();
+            yaw = camera.getYaw();
+            zoomFactor = camera.getZoomFactor();
+        }
+
+        panorama.getCamera().lookAtAndZoomFactor(pitch, yaw, zoomFactor, false);
+        plManager.setPanorama(panorama);
+        currentIndex = index;
     }
 }
