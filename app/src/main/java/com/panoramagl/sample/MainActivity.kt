fa29be3ff3773a6fdf44b8396e03aa6513bfbc58
Hannes Achleitner <hannes.software@gmx.at>
2021-07-03 20:46:18 +0200

    Minor Kotlin change

--- app/src/main/java/com/panoramagl/sample/MainActivity.kt
+++ app/src/main/java/com/panoramagl/sample/MainActivity.kt
@@ -1,44 +1,44 @@
 package com.panoramagl.sample
 
-import androidx.appcompat.app.AppCompatActivity
-import com.panoramagl.hotspots.HotSpotListener
-import com.panoramagl.PLManager
-import android.os.Bundle
-import android.view.ViewGroup
-import android.view.MotionEvent
-import com.panoramagl.utils.PLUtils
-import com.panoramagl.PLSphericalPanorama
-import com.panoramagl.PLImage
-import com.panoramagl.hotspots.ActionPLHotspot
 import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
-import com.panoramagl.PLConstants
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.panoramagl.PLConstants
+import com.panoramagl.PLImage
+import com.panoramagl.PLManager
+import com.panoramagl.PLSphericalPanorama
+import com.panoramagl.hotspots.ActionPLHotspot
+import com.panoramagl.hotspots.HotSpotListener
+import com.panoramagl.utils.PLUtils
 
 class MainActivity : AppCompatActivity(), HotSpotListener {
 
-    private var plManager: PLManager? = null
+    private lateinit var plManager: PLManager
     private var currentIndex = -1
     private val resourceIds = intArrayOf(R.raw.sighisoara_sphere, R.raw.sighisoara_sphere_2)
     private val buttonClickListener = View.OnClickListener { v ->
         when (v.id) {
             R.id.button_1 -> changePanorama(0)
             R.id.button_2 -> changePanorama(1)
-            else -> {
-            }
+            else -> Unit
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        plManager = PLManager(this)
-        plManager!!.setContentView(findViewById<View>(R.id.content_view) as ViewGroup)
-        plManager!!.onCreate()
-        plManager!!.isAccelerometerEnabled = false
-        plManager!!.isInertiaEnabled = false
-        plManager!!.isZoomEnabled = false
+        plManager = PLManager(this).apply {
+            setContentView(findViewById<View>(R.id.content_view) as ViewGroup)
+            onCreate()
+            isAccelerometerEnabled = false
+            isInertiaEnabled = false
+            isZoomEnabled = false
+        }
         changePanorama(0)
         val button1 = findViewById<Button>(R.id.button_1)
         val button2 = findViewById<Button>(R.id.button_2)
@@ -48,25 +48,26 @@ class MainActivity : AppCompatActivity(), HotSpotListener {
 
     override fun onResume() {
         super.onResume()
-        plManager!!.onResume()
+        plManager.onResume()
     }
 
     override fun onPause() {
-        plManager!!.onPause()
+        plManager.onPause()
         super.onPause()
     }
 
     override fun onDestroy() {
-        plManager!!.onDestroy()
+        plManager.onDestroy()
         super.onDestroy()
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        return plManager!!.onTouchEvent(event)
+        return plManager.onTouchEvent(event)
     }
 
     private fun changePanorama(index: Int) {
-        if (currentIndex == index) return
+        if (currentIndex == index)
+            return
         val image3D = PLUtils.getBitmap(this, resourceIds[index])
         val panorama = PLSphericalPanorama()
         panorama.setImage(PLImage(image3D, false))
@@ -74,7 +75,7 @@ class MainActivity : AppCompatActivity(), HotSpotListener {
         var yaw = 0f
         var zoomFactor = 0.7f
         if (currentIndex != -1) {
-            plManager!!.panorama?.camera?.apply {
+            plManager.panorama?.camera?.apply {
                 pitch = this.pitch
                 yaw = this.yaw
                 zoomFactor = this.zoomFactor
@@ -90,7 +91,7 @@ class MainActivity : AppCompatActivity(), HotSpotListener {
         panorama.addHotspot(plHotspot1)
         panorama.addHotspot(plHotspot2)
         panorama.camera.lookAtAndZoomFactor(pitch, yaw, zoomFactor, false)
-        plManager!!.panorama = panorama
+        plManager.panorama = panorama
         currentIndex = index
     }
 
