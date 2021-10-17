fa83478863d1dde491e455e30b112bf09ec21035
Hannes Achleitner <hannes.software@gmx.at>
2021-10-17 16:35:57 +0200

    Emulator uses alwasy PLOpenGLVersion1_0

--- library/src/main/java/com/panoramagl/utils/VersionUtils.kt
+++ library/src/main/java/com/panoramagl/utils/VersionUtils.kt
@@ -4,26 +4,25 @@ import android.os.Build
 import com.panoramagl.enumerations.PLOpenGLVersion
 import javax.microedition.khronos.opengles.GL10
 
-private fun getAndroidVersion() = Build.VERSION.RELEASE.trim().split(".")[0].toInt()
+private var versionGL: String? = null
 
 fun isEmulator() = Build.PRODUCT.contains("sdk")
 
 fun GL10.isHigherThanOpenGL1() = this.getOpenGLVersion().ordinal > PLOpenGLVersion.PLOpenGLVersion1_0.ordinal
 
 fun GL10.getOpenGLVersion(): PLOpenGLVersion {
-    val versionGL = this.glGetString(GL10.GL_VERSION)
+    if (versionGL == null) {
+        versionGL = this.glGetString(GL10.GL_VERSION)
+    }
     val version = if (isEmulator()) {
-        (if (getAndroidVersion() < 3)
-            PLOpenGLVersion.PLOpenGLVersion1_0
-        else
-            PLOpenGLVersion.PLOpenGLVersion1_1)
+        PLOpenGLVersion.PLOpenGLVersion1_0
     } else {
         when {
-            versionGL.contains("1.0") -> PLOpenGLVersion.PLOpenGLVersion1_0
-            versionGL.contains("1.1") -> PLOpenGLVersion.PLOpenGLVersion1_1
-            versionGL.contains("2.0") -> PLOpenGLVersion.PLOpenGLVersion2_0
-            versionGL.contains("3.0") -> PLOpenGLVersion.PLOpenGLVersion3_0
-            versionGL.contains("3.1") -> PLOpenGLVersion.PLOpenGLVersion3_1
+            versionGL!!.contains("1.0") -> PLOpenGLVersion.PLOpenGLVersion1_0
+            versionGL!!.contains("1.1") -> PLOpenGLVersion.PLOpenGLVersion1_1
+            versionGL!!.contains("2.0") -> PLOpenGLVersion.PLOpenGLVersion2_0
+            versionGL!!.contains("3.0") -> PLOpenGLVersion.PLOpenGLVersion3_0
+            versionGL!!.contains("3.1") -> PLOpenGLVersion.PLOpenGLVersion3_1
             else -> PLOpenGLVersion.PLOpenGLVersion3_1
         }
     }
