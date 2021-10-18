806b43bad424cf4163aed62fe1073f0e0d108614
Hannes Achleitner <hannes.software@gmx.at>
2021-10-18 13:32:17 +0200

    Remove obsolete Android checks
    We use minSdk 14, and checks for Android 3 are pointless

--- library/src/main/java/com/panoramagl/utils/VersionUtils.kt
+++ library/src/main/java/com/panoramagl/utils/VersionUtils.kt
@@ -2,6 +2,4 @@ package com.panoramagl.utils
 
 import android.os.Build
 
-fun getAndroidVersion() = Build.VERSION.RELEASE.trim().split(".")[0].toInt()
-
 fun isEmulator() = Build.PRODUCT.contains("sdk")
