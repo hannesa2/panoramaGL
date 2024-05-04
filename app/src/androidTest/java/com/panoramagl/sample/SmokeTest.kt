bc98710a8fb9de33eeea0e74d6196219ea14f30a
Hannes Achleitner <hannes.software@gmx.at>
2024-05-04 14:00:18 +0200

    Cleanup tests

--- app/src/androidTest/java/com/panoramagl/sample/SmokeTest.kt
+++ app/src/androidTest/java/com/panoramagl/sample/SmokeTest.kt
@@ -31,9 +31,9 @@ class SmokeTest {
         Thread.sleep(100)
         takeScreenshot().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-start")
 
-        Espresso.onView(withId(R.id.button_1)).perform(ViewActions.click())
+        onView(withId(R.id.button_1)).perform(ViewActions.click())
         takeScreenshot().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-button1")
-        Espresso.onView(withId(R.id.button_2)).perform(ViewActions.click())
+        onView(withId(R.id.button_2)).perform(ViewActions.click())
         takeScreenshot().writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-button2")
     }
 
